aa379626a0eb85d5557cefc877815d1a5e541552
harsh-sharma02 <harshofficial2004@gmail.com>
2025-07-01 16:57:46 +0530

    Create grbl_config.h

---
+++ grbl_config.h
@@ -0,0 +1,74 @@
+#ifndef config_h
+#define config_h
+#include "grbl.h" 
+
+#define DEFAULTS_GENERIC
+
+#define BAUD_RATE 115200
+
+#define CPU_MAP_ATMEGA328P
+
+#define CMD_STATUS_REPORT '?'
+#define CMD_FEED_HOLD '!'
+#define CMD_CYCLE_START '~'
+#define CMD_RESET 0x18 // ctrl-x.
+#define CMD_SAFETY_DOOR '@'
+
+#define HOMING_INIT_LOCK // Comment to disable
+
+#define HOMING_CYCLE_0 ((1<<X_AXIS)|(1<<Y_AXIS))
+
+#define N_HOMING_LOCATE_CYCLE 1 // Integer (1-128)
+
+#define N_STARTUP_LINE 2 // Integer (1-2)
+
+#define N_DECIMAL_COORDVALUE_INCH 4 // Coordinate or position value in inches
+#define N_DECIMAL_COORDVALUE_MM   3 // Coordinate or position value in mm
+#define N_DECIMAL_RATEVALUE_INCH  1 // Rate or velocity value in in/min
+#define N_DECIMAL_RATEVALUE_MM    0 // Rate or velocity value in mm/min
+#define N_DECIMAL_SETTINGVALUE    3 // Decimals for floating point setting values
+
+#define MESSAGE_PROBE_COORDINATES // Enabled by default. Comment to disable.
+
+#define COREXY
+
+#define SAFETY_DOOR_SPINDLE_DELAY 4000
+#define SAFETY_DOOR_COOLANT_DELAY 1000
+
+#define ACCELERATION_TICKS_PER_SECOND 100 
+
+#define ADAPTIVE_MULTI_AXIS_STEP_SMOOTHING  // Default enabled. Comment to disable.
+
+#define TOOL_LENGTH_OFFSET_AXIS Z_AXIS // Default z-axis. Valid values are X_AXIS, Y_AXIS, or Z_AXIS.
+
+#define VARIABLE_SPINDLE // Default enabled. Comment to disable.
+
+#define SPINDLE_MAX_RPM 90 // Max spindle RPM. This value is equal to 100% duty cycle on the PWM.
+#define SPINDLE_MIN_RPM 0.0    // Min spindle RPM. This value is equal to (1/256) duty cycle on the PWM.
+
+#define MINIMUM_JUNCTION_SPEED 0.0 // (mm/min)
+
+#define MINIMUM_FEED_RATE 1.0 // (mm/min)
+
+#define N_ARC_CORRECTION 12 // Integer (1-255)
+
+#define ARC_ANGULAR_TRAVEL_EPSILON 5E-7 // Float (radians)
+
+#define DWELL_TIME_STEP 50 // Integer (1-255) (milliseconds)
+
+#ifndef HOMING_CYCLE_0
+  #error "Required HOMING_CYCLE_0 not defined."
+#endif
+
+#if defined(USE_SPINDLE_DIR_AS_ENABLE_PIN) && !defined(VARIABLE_SPINDLE)
+  #error "USE_SPINDLE_DIR_AS_ENABLE_PIN may only be used with VARIABLE_SPINDLE enabled"
+#endif
+
+#if defined(USE_SPINDLE_DIR_AS_ENABLE_PIN) && !defined(CPU_MAP_ATMEGA328P)
+  #error "USE_SPINDLE_DIR_AS_ENABLE_PIN may only be used with a 328p processor"
+#endif
+
+// ---------------------------------------------------------------------------------------
+
+
+#endif
